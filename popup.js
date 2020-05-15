41067257e78253330c6dc7b6835a38228e394d3e
AbelWeldaregay <abelweldaregay@gmail.com>
2020-05-15 16:36:10 -0400

    redirect to banner page and update ui

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var btn = document.getElementById('redirect-button');
+    btn.addEventListener('click', function() {
+       alert("button clicked");
+    });
+});
