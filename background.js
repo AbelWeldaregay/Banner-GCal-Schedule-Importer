97855c4218564adaabfed4f44e0b7d5c174eec8c
AbelWeldaregay <abelweldaregay@gmail.com>
2020-05-15 21:50:49 -0400

    add initial extract script

--- background.js
+++ background.js
@@ -1,1 +1,12 @@
-console.log("background running");
+// opens a communication port
+chrome.runtime.onConnect.addListener(function(port) {
+	//listen for every message passing through it
+	port.onMessage.addListener(function(o) {
+		// if the message comes from the popup
+		if (o.from && o.from === "popup" && o.start && o.start === "scrap_web") {
+			chrome.tabs.executeScript({
+				file: "extract_schedule.js"
+			});
+		}
+	});
+});
--- popup.js
+++ popup.js
@@ -2,7 +2,16 @@ chrome.storage.local.get("prefs", function(data) {
 	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
 	    let url = tabs[0].url;
 	   	if (data.prefs == "import" && url.indexOf("StudentRegistrationSsb") > -1) {
+	   		// opens a communication between scripts
+	   		var port = chrome.runtime.connect();
+	   		
 	   		document.getElementById("redirect-button").textContent = "Import Schedule";
+	   		var container = document.getElementById("scheduleListViewWrapper");
+	   		
+	   		port.postMessage({
+	   			"from": "popup",
+	   			"start": "scrap_web"
+	   		});
 	   	}
 	    // use `url` here inside the callback because it's asynchronous!
 	});
