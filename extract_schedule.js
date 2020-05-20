59943f80e80a3efc444526f4d8b9c5bc3e7d3d25
AbelWeldaregay <abelweldaregay@gmail.com>
2020-05-20 18:02:38 -0400

    import schedule to google calander when import is clicked

--- extract_schedule.js
+++ extract_schedule.js
@@ -3,20 +3,23 @@ function extract_schedule() {
 	console.log("extract_schedule invoked");
 	var schedule_wrappers = document.getElementById("scheduleListViewWrapper");
 	var child_divs = schedule_wrappers.getElementsByTagName("div")[0].getElementsByClassName("listViewWrapper");
+	var selected_semester = document.getElementsByClassName("select2-hidden-accessible")[0].innerHTML;
 	var schedule = [];
 	// var meeting_info = child_divs.scheduleListView.getElementsByClassName("listViewMeetingInformation");
 	for (var i = 0; i < child_divs.length; ++i) {
 		var meeting_days = child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByClassName("ui-pillbox-summary")[0].innerText.split(",")[0];
 		if (meeting_days === "None") {
 			schedule.push({
-				 "course_title"	   : child_divs[i].getElementsByClassName("list-view-course-title")[0].innerText,
-				 "instructor_name" : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[0].split(":")[1],
-				 "course_crn"      : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[1].split(":")[1],
-	 			 "meeting_window"  : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[0].innerText.replace(/\s/g,'').split("--"),
-				 "meeting_days"    : "Online",
-				 "meeting_times"   : "Online",
-				 "meeting_building": "Online",
-				 "meeting_room"    : "Online"
+				 "course_title"	    : child_divs[i].getElementsByClassName("list-view-course-title")[0].innerText,
+				 "instructor_name"  : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[0].split(":")[1],
+				 "course_crn"       : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[1].split(":")[1],
+	 			 "meeting_window"   : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[0].innerText.replace(/\s/g,'').split("--"),
+				 "selected_semester": selected_semester,
+				 "meeting_days"     : "Online",
+				 "meeting_times"    : "Online",
+				 "meeting_building" : "Online",
+				 "meeting_room"     : "Online",
+				 "course_type"	    : "Online"
 			});
 		} else {
 			schedule.push({
@@ -27,7 +30,9 @@ function extract_schedule() {
 				 "meeting_days"    : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByClassName("ui-pillbox-summary")[0].innerText.split(","),
 				 "meeting_times"   : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[2].innerText.replace(/\s/g,'').split("-"),
 				 "meeting_building": child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerText.split(":")[5].replace("Room",""),
-				 "meeting_room"    : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerText.split(":")[6]
+				 "meeting_room"    : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerText.split(":")[6],
+				 "course_type"	   : "In-Person",
+				 "selected_semester": selected_semester
 			});
 		}
 		chrome.storage.local.set({"schedule": schedule}, function() {
--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
+var importButtonHTML = '<button id="import-button" class="btn red accent-4">Import Schedule</button>';
+var authenticateButtonHTML = '<button id="authenticate-button" class="btn red accent-4" style="letter-spacing: 0px;">Allow Google Calendar Access</button>';
+var disabledAuthenticateButtonHTML = '<button id="authenticate-button" class="btn red accent-4" style="margin: 5px 0; letter-spacing: 0px;" disabled>Allow Google Calendar Access</button>';
+var testudoLinkButtonHTML = '<button id="testudo-link-button" class="btn red accent-4">Take me to Testudo!</button>';
+var exportToIcsButtonHTML = '<button id="export-ics-button" class="btn red accent-4" style="margin: 5px 0;letter-spacing: 0px;">Export schedule to .ics format</button>';
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 	update_table();
@@ -31,7 +36,11 @@ function update_table() {
 	document.getElementById("schedule").innerHTML = "";
 	// opens a communication between scripts
 	document.getElementById("banner-example-image").style.display = "none";
+	courseEventInfo = null;
 	chrome.storage.local.get("schedule", function(schedule) {
+		courseEventInfo = schedule["schedule"];		
+		const semFirstDate = courseEventInfo[0].meeting_window[0];
+		const semLastDate = courseEventInfo[0].meeting_window[1];
 		for (var i = 0; i < schedule["schedule"].length; ++i) {
 	   		table_str += "<tr>";
 	   		table_str += "<td>"+schedule["schedule"][i]["course_title"]+ "(CRN: " + schedule["schedule"][i]["course_crn"] + ")" + "</td>";
@@ -61,13 +70,142 @@ function update_table() {
 
 	chrome.identity.getAuthToken({"interactive": true}, function (token) {
 		if (token == null) {
-			console.log("its null");
+			const authBtnEl = document.querySelector('#button-div');
+			console.log("direct google import not available");
 		} else {
-			console.log(token);
+          document.querySelector('#button-div').innerHTML = importButtonHTML;
+          // Add event listener for import schedule button
+          var importScheduleButton = document.getElementById('import-button');
+          importScheduleButton.addEventListener('click', function () {
+            console.log("importScheduleButton has been clicked.");
+            //_gaq.push(['_trackEvent', 'importScheduleButton', 'clicked']);
+
+            // chrome.identity.removeCachedAuthToken(
+            //       { 'token': access_token },
+            //       getTokenAndXhr);
+
+            // Initiate GCal scheduling functionality
+            importSchedule(courseEventInfo, courseEventInfo[0].selected_semester, courseEventInfo[0].meeting_window[1]);
+          }, false);
+
 		}
 	})
 
 
+}
+
+function importSchedule(courseEventInfo, viewedSemester, semEndDate) {
+  document.querySelector('#import-button').className += " disabled";
+  var pagecodediv = document.querySelector('#pagecodediv');
+  chrome.identity.getAuthToken({
+    'interactive': true
+  }, function (token) {
+    // Use the token.
+    console.log(token);
+    // POST request to create a new calendar
+    var url = "https://www.googleapis.com/calendar/v3/calendars";
+    var params = {
+      "summary": viewedSemester + " ODU Schedule",
+      "timeZone": "America/New_York"
+    };
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          var newCalId = (JSON.parse(xhr.responseText).id);
+          pagecodediv.innerText = 'Importing your schedule...';
+          document.querySelector('#import-button').remove();
+          importEvents(newCalId, token, courseEventInfo, semEndDate);
+        } else {
+          console.log("Error", xhr.statusText);
+          pagecodediv.innerText = 'Uh Oh! Something went wrong...Sorry about the inconvenience! Feel free to shoot abelweldaregay@gmail.com an email so we know we\'re down!';
+          document.querySelector('#import-button').remove();
+        }
+      }
+    }
+
+    xhr.send(JSON.stringify(params));
+  });
+}
+
+
+function importEvents(calId, token, courseEventInfo, semEndDate) {
+  var semEndDateParam = new Date(semEndDate);
+
+  semEndDateParam.setDate(semEndDateParam.getDate() + 1);
+  semEndDateParamStr = semEndDateParam.toJSON().substr(0, 4) + semEndDateParam.toJSON().substr(5, 2) + semEndDateParam.toJSON().substr(8, 2);
+  var postImportActionsCalled = false;
+
+
+  for (var i = 0; i < courseEventInfo.length; i++) {
+    // POST request to create a new event
+    var url = "https://www.googleapis.com/calendar/v3/calendars/" + calId + "/events";
+
+    var course = courseEventInfo[i];
+
+    // Set start/end dates taking into consideration am/pm
+    var startDate = (new Date(course.meeting_window[0]));
+    startDate.setHours(course.meeting_times[0].substring(0, 2));
+    startDate.setMinutes(course.meeting_times[0].substring(3, 5));
+    if (parseInt(startDate.getHours()) < 12) {
+      startDate.setHours(startDate.getHours() + 12);
+    }
+    var endDate = (new Date(course.meeting_window[0]));
+    endDate.setHours(course.meeting_times[1].substring(0, 2));
+    endDate.setMinutes(course.meeting_times[1].substring(3, 5));
+    if (parseInt(endDate.getHours()) < 12) {
+      endDate.setHours(endDate.getHours() + 12);
+    }
+
+    var params = {
+      "summary": course.course_title + " (" + course.course_type + ")",
+      "location": course.meeting_building ,
+      "description": "CRN: " + course.course_crn,
+      "start": {
+        "dateTime": startDate.toJSON(),
+        "timeZone": "America/New_York"
+      },
+      "end": {
+        "dateTime": endDate.toJSON(),
+        "timeZone": "America/New_York"
+      },
+      "recurrence": [
+        "RRULE:FREQ=WEEKLY;UNTIL=" + semEndDateParamStr
+      ]
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == XMLHttpRequest.DONE && !postImportActionsCalled) {
+        // console.log(JSON.parse(xhr.responseText));
+        postImportActions();
+        postImportActionsCalled = true;
+      }
+    }
+
+    xhr.send(JSON.stringify(params));
+  }
+}
+
+// After schedule has been imported
+function postImportActions() {
+  console.log("Finished importing courses");
+  console.log(pagecodediv);
+  // pagecodediv.innerText = 'Completed schedule import.';
+
+  window.open('https://calendar.google.com/calendar/render#main_7%7Cmonth', '_blank');
 }
 
 document.addEventListener('DOMContentLoaded', function () {
