e980251b9935171664ff5d53620f18c6e18905d0
AbelWeldaregay <abelweldaregay@gmail.com>
2020-05-27 22:21:51 -0400

    remove repeating code

--- extract_schedule.js
+++ extract_schedule.js
@@ -16,22 +16,27 @@ function extract_schedule() {
 	} else {
 		for (var i = 0; i < child_divs.length; ++i) {
 			var meeting_days = child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByClassName("ui-pillbox-summary")[0].innerText.split(",");
+			var course_title = course_names[i+1].getElementsByTagName("td")[1].innerText.substring(0, course_names[i+1].getElementsByTagName("td")[1].innerText.indexOf(","));
+			var instructor_name = child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[0].split(":")[1];
+			course_crn = child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[1].split(":")[1];
 			if (meeting_days[0] === "None") {
 				schedule.push({
-					 "course_title"	    : course_names[i+1].getElementsByTagName("td")[1].innerText.substring(0, course_names[i+1].getElementsByTagName("td")[1].innerText.indexOf(",")),
-					 "instructor_name"  : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[0].split(":")[1],
-					 "course_crn"       : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[1].split(":")[1],
+					 "course_title"	    : course_title,
+					 "instructor_name"  : instructor_name,
+					 "course_crn"       : course_crn,
 		 			 "meeting_window"   : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[0].innerText.replace(/\s/g,'').split("--"),
 					 "selected_semester": selected_semester,
 					 "meeting_times"    : "Online"
 				});
 			} else {
 				var meeting_window = child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[0].innerText.replace(/\s/g,'').split("--");
+				var meeting_times = child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[2].innerText.replace(/\s/g,'').split("-");
 				var semFirstDay = new Date(meeting_window[0]);
 				semFirstDay = semFirstDay.getDay();	
 				var classStartDay = 0;
 				var classStartDate = new Date(new Date(meeting_window[0]));
 				var classEndDate = new Date(new Date(meeting_window[0]));
+				var meeting_building = child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerText.split(":")[5].replace("Room","").trim();
 				if (child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerHTML.split("<br>").length > 2) {
 					var sub_courses_length = child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByClassName("list-view-pillbox").length;
 					for (var x = 0; x < sub_courses_length; ++x) {
@@ -39,13 +44,13 @@ function extract_schedule() {
 						var meeting_room = child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerHTML.split("<br>")[x].substr(-4).replace(/\D/g, "");
 						for (var j = 0; j < meeting_days.length; ++j) {
 							schedule.push({
-								 "course_title"	    : course_names[i+1].getElementsByTagName("td")[1].innerText.substring(0, course_names[i+1].getElementsByTagName("td")[1].innerText.indexOf(",")),
-								 "instructor_name"  : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[0].split(":")[1],
-								 "course_crn"       : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[1].split(":")[1],
-					 			 "meeting_window"   : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[0].innerText.replace(/\s/g,'').split("--"),
+								 "course_title"	    : course_title,
+								 "instructor_name"  : instructor_name,
+								 "course_crn"       : course_crn,
+					 			 "meeting_window"   : meeting_window,
 								 "meeting_days"     : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByClassName("ui-pillbox-summary")[x].innerText.split(","),
-								 "meeting_times"    : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[2].innerText.replace(/\s/g,'').split("-"),
-								 "meeting_building" : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerText.split(":")[5].replace("Room","").trim(),
+								 "meeting_times"    : meeting_times,
+								 "meeting_building" : meeting_building,
 								 "meeting_room"     : meeting_room,
 								 "course_type"	    : "In-Person",
 								 "selected_semester": selected_semester,
@@ -57,13 +62,13 @@ function extract_schedule() {
 					for (var k = 0; k < meeting_days.length; ++k) {
 				
 						schedule.push({
-							 "course_title"	   : course_names[i+1].getElementsByTagName("td")[1].innerText.substring(0, course_names[i+1].getElementsByTagName("td")[1].innerText.indexOf(",")),
-							 "instructor_name" : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[0].split(":")[1],
-							 "course_crn"      : child_divs[i].getElementsByClassName("listViewInstructorInformation")[0].innerText.split("\n")[1].split(":")[1],
-				 			 "meeting_window"  : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[0].innerText.replace(/\s/g,'').split("--"),
-							 "meeting_days"    : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByClassName("ui-pillbox-summary")[0].innerText.split(","),
-							 "meeting_times"   : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].getElementsByTagName("span")[2].innerText.replace(/\s/g,'').split("-"),
-							 "meeting_building": child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerText.split(":")[5].replace("Room",""),
+							 "course_title"	   : course_title,
+							 "instructor_name" : instructor_name,
+							 "course_crn"      : course_crn,
+				 			 "meeting_window"  : meeting_window,
+							 "meeting_days"    : meeting_days,
+							 "meeting_times"   : meeting_times,
+							 "meeting_building": meeting_building,
 							 "meeting_room"    : child_divs[i].getElementsByClassName("listViewMeetingInformation")[0].innerText.split(":")[6],
 							 "course_type"	   : "In-Person",
 							 "selected_semester": selected_semester,
